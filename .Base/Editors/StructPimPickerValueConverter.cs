0288ee60b5feae0b3f28c021b8734232fa7dfe7f
Jan Rasmussen <jan@struct.dk>
2023-02-28 10:35:37 +0100

    add propertyvalueconverter

---
+++ .Base/Editors/StructPimPickerValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Struct.Umbraco.StructPimPicker.Models;
+using System.Text.Json.Nodes;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+
+namespace Struct.Umbraco.StructPimPicker._Base.Editors
+{
+    public class StructPimPickerValueConverter : PropertyValueConverterBase
+    {
+        public StructPimPickerValueConverter()
+        {
+        }
+
+        public override bool IsConverter(IPublishedPropertyType propertyType) => propertyType.EditorAlias.Equals("Struct.Umbraco.StructPimPicker");
+
+        public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) => PropertyCacheLevel.Snapshot;
+
+        public override Type GetPropertyValueType(IPublishedPropertyType propertyType) => typeof(List<PimItemReference>);
+
+        public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            if (!string.IsNullOrEmpty(source?.ToString()))
+                return JsonConvert.DeserializeObject<List<PimItemReference>>(source.ToString());
+
+            return null;
+        }
+    }
+}
---
+++ Models/PimItemReference.cs
@@ -0,0 +1,8 @@
+﻿namespace Struct.Umbraco.StructPimPicker.Models
+{
+    public class PimItemReference
+    {
+        public string ItemId { get; set; }
+        public Struct.PIM.Api.Models.DataConfiguration.EntityType ReferenceType { get; set; }
+    }
+}
